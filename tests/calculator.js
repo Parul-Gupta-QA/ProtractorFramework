0f9f01ac5bbbfdb4723a5ec57e0a4b8ff8274e81
Parul <parul_gupta85@yahoo.co.in>
2021-03-10 13:43:27 -0500

    changed cal

--- tests/calculator.js
+++ tests/calculator.js
@@ -29,40 +29,11 @@ describe('demo calculator tests', function() {
 
        homepage.verifyresult('7');
 
-        browser.sleep(2000);
+        browser.sleep(3000);
        
     });
 
-    it('addition test', function() {
-      
-        //browser.get('https://juliemr.github.io/protractor-demo/');
-
-        homepage.get('https://juliemr.github.io/protractor-demo/');
-        
-        
-        //element(by.model('first')).sendKeys('2');
-
-        homepage.enterFirstNumber('4');
-
-       // element(by.model('second')).sendKeys('3');
-
-       homepage.enterSecondNumber('3');
-
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-
-        homepage.clickGo();
-
-
-       //let result = element(by.cssContainingText('.ng-binding','5'));
-
-       //expect(result.getText()).toEqual('5');
-
-       homepage.verifyresult('7');
-
-        browser.sleep(2000);
-       
-    }); 
+    
     
 
        
